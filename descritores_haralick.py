650ac3510a61fcbe23859a4fd827cca1943bae13
phlc <pedrocarvalho@outlook.com>
2022-05-05 12:50:02 -0300

    descritores_haralick.py comentado

--- descritores_haralick.py
+++ descritores_haralick.py
@@ -1,6 +1,9 @@
 from math import log
 import numpy as np
 
+# Calcúla Entropia de Haralick
+# @param matriz de co-ocorrência
+# @return entropia
 def entropia(matriz):
     resultado = 0.0
     for linha in range(len(matriz)):
@@ -8,6 +11,9 @@ def entropia(matriz):
            resultado += matriz[linha][coluna] * log(matriz[linha][coluna], 2)
     return -resultado
 
+# Calcúla Homogeneidade de Haralick
+# @param matriz de co-ocorrência
+# @return homogeneidade
 def homogeneidade(matriz):
     resultado = 0.0
     for linha in range(len(matriz)):
@@ -15,6 +21,9 @@ def homogeneidade(matriz):
             resultado += matriz[linha][coluna] / (1 + abs(linha - coluna))
     return resultado
 
+# Calcúla Energia de Haralick
+# @param matriz de co-ocorrência
+# @return energia
 def energia(matriz):
     resultado = 0.0
     for linha in range(len(matriz)):
@@ -22,6 +31,9 @@ def energia(matriz):
             resultado+= matriz[linha][coluna]**2
     return resultado
 
+# Calcúla Contraste de Haralick
+# @param matriz de co-ocorrência
+# @return contraste
 def contraste(matriz):
     resultado = 0.0
     for linha in range(len(matriz)):
