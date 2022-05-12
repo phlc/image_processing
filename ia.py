6bb23d71d36b1ff36afba5bcd60bdbd9e169f150
Ana Laura Fernandes <analaura.fernandes@dtidigital.com.br>
2022-05-12 16:04:26 -0300

    Modularização da rede neural

--- ia.py
+++ ia.py
@@ -4,77 +4,91 @@ import numpy as np
 import pickle
 import random
 
-input = open('dataset.pkl', 'rb')
-dataset = np.array(pickle.load(input))
-input.close()
 
-birad1 = []
-birad2 = []
-birad3 = []
-birad4 = []
+def preparar_rede_neural():
+    input = open('dataset.pkl', 'rb')
+    dataset = np.array(pickle.load(input))
+    input.close()
 
-for instance in range(100):
-    birad1.append([np.reshape(dataset[0][instance], 15), [1,0,0,0]])
-    birad2.append([np.reshape(dataset[1][instance], 15), [0,1,0,0]])
-    birad3.append([np.reshape(dataset[2][instance], 15), [0,0,1,0]])
-    birad4.append([np.reshape(dataset[3][instance], 15), [0,0,0,1]])
+    birad1 = []
+    birad2 = []
+    birad3 = []
+    birad4 = []
 
-random.shuffle(birad1)
-random.shuffle(birad2)
-random.shuffle(birad3)
-random.shuffle(birad4)
+    for instance in range(100):
+        birad1.append([np.reshape(dataset[0][instance], 15), [1,0,0,0]])
+        birad2.append([np.reshape(dataset[1][instance], 15), [0,1,0,0]])
+        birad3.append([np.reshape(dataset[2][instance], 15), [0,0,1,0]])
+        birad4.append([np.reshape(dataset[3][instance], 15), [0,0,0,1]])
 
-training_data = []
-test_data = []
+    random.shuffle(birad1)
+    random.shuffle(birad2)
+    random.shuffle(birad3)
+    random.shuffle(birad4)
 
-for instance in range(75):
-    training_data.append(birad1[instance])
-    training_data.append(birad2[instance])
-    training_data.append(birad3[instance])
-    training_data.append(birad4[instance])
+    training_data = []
+    test_data = []
 
-training_data = np.array(training_data, dtype=object)
+    for instance in range(75):
+        training_data.append(birad1[instance])
+        training_data.append(birad2[instance])
+        training_data.append(birad3[instance])
+        training_data.append(birad4[instance])
 
-for instance in range(75, 100):
-    test_data.append(birad1[instance])
-    test_data.append(birad2[instance])
-    test_data.append(birad3[instance])
-    test_data.append(birad4[instance])
+    training_data = np.array(training_data, dtype=object)
 
-test_data = np.array(test_data, dtype=object)
+    for instance in range(75, 100):
+        test_data.append(birad1[instance])
+        test_data.append(birad2[instance])
+        test_data.append(birad3[instance])
+        test_data.append(birad4[instance])
 
-random.shuffle(training_data)
-random.shuffle(test_data)
+    test_data = np.array(test_data, dtype=object)
 
-train_X = []
-train_y = []
+    random.shuffle(training_data)
+    random.shuffle(test_data)
 
-for descritor, classe in training_data:
-    train_X.append(descritor)
-    train_y.append(classe)
+    train_X = []
+    train_y = []
 
-train_X = np.array(train_X)
-train_y = np.array(train_y)
+    for descritor, classe in training_data:
+        train_X.append(descritor)
+        train_y.append(classe)
 
-test_X = []
-test_y = []
+    train_X = np.array(train_X)
+    train_y = np.array(train_y)
 
-for descritor, classe in test_data:
-    test_X.append(descritor)
-    test_y.append(classe)
+    test_X = []
+    test_y = []
 
-test_X = np.array(test_X)
-test_y = np.array(test_y)
+    for descritor, classe in test_data:
+        test_X.append(descritor)
+        test_y.append(classe)
+
+    test_X = np.array(test_X)
+    test_y = np.array(test_y)
+
+    pickle.dump(train_X, 'train_x.pkl')
+    pickle.dump(train_y, 'train_y.pkl')
+    pickle.dump(test_X, 'test_X.pkl')
+    pickle.dump(test_y, 'test_y.pkl')
 
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(42, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(4, activation=tf.nn.softmax))
+def treinar_rede_neural():
+    train_X = open('train_X.pkl', 'rb')
+    train_y = open('train_y.pkl', 'rb')
+    test_X = open('test_X.pkl', 'rb')
+    test_y = open('test_y.pkl', 'rb')
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(42, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(4, activation=tf.nn.softmax))
 
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy', tf.keras.metrics.Precision()])
-model.fit(x=train_X, y=train_y, epochs=500)
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy', tf.keras.metrics.Precision()])
+    model.fit(x=train_X, y=train_y, epochs=500)
 
-model.evaluate(x=test_X, y=test_y)
+    model.evaluate(x=test_X, y=test_y)
+
+    return(model)
 
 # Salvar rede neural
 # rede_file = open('rede_neural.pkl',  'wb')
