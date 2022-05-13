f3dd21a5bf538c97bf49c7f65bb76d225006826b
phlc <pedrocarvalho@outlook.com>
2022-05-13 09:28:19 -0300

    alteracoes discord

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ class main:
 
         menuRede = Menu(menu)
         menu.add_cascade(label='Rede Neural', menu=menuRede)
-        menuRede.add_command(label='Treinar', command=self.selecionar_diretorio_imagens) 
-        menuRede.add_command(label='Testar', command=self.selecionar_diretorio_imagens) 
+        menuRede.add_command(label='Treinar', command=self.realizar_treino_rede_neural) 
+        menuRede.add_command(label='Testar', command=self.testar_rede_neural) 
 
         menuSVM = Menu(menu)
         menu.add_cascade(label='SVM', menu=menuSVM)
@@ -87,7 +87,7 @@ class main:
         self.botao_classificar_svm = Button(self.frame_inferior, text='Classificar (SVM)', width=15, command=self.classificar_imagem_svm )
         self.botao_classificar_svm.grid(row=1, column=4, padx=10, pady=5)
 
-        self.botao_classificar_rede = Button(self.frame_inferior, text='Classificar (RN)', width=15, command=lambda: self.exibir_descritores_imagem(imagePath=self.caminhoDaImagem))
+        self.botao_classificar_rede = Button(self.frame_inferior, text='Classificar (RN)', width=15, command=self.classificar_imagem_rede_neural )
         self.botao_classificar_rede.grid(row=0, column=4, padx=10, pady=5)
 
 
@@ -172,15 +172,15 @@ class main:
 
         # Populando a tabela com os dados dos descritores
         tabelaDeDescritores.insert(parent='',index='end',iid=0,text='',
-        values=('C1',descritores[0][0],descritores[0][1],descritores[0][2], descritores[0][3] ))
+        values=('C1',descritores[0][0],descritores[0][1],descritores[0][2]))
         tabelaDeDescritores.insert(parent='',index='end',iid=1,text='',
-        values=('C2',descritores[1][0],descritores[1][1],descritores[1][2], descritores[1][3]))
+        values=('C2',descritores[1][0],descritores[1][1],descritores[1][2]))
         tabelaDeDescritores.insert(parent='',index='end',iid=2,text='',
-        values=('C4',descritores[2][0],descritores[2][1],descritores[2][2], descritores[2][3]))
+        values=('C4',descritores[2][0],descritores[2][1],descritores[2][2]))
         tabelaDeDescritores.insert(parent='',index='end',iid=3,text='',
-        values=('C8',descritores[3][0],descritores[3][1],descritores[3][2], descritores[3][3]))
+        values=('C8',descritores[3][0],descritores[3][1],descritores[3][2]))
         tabelaDeDescritores.insert(parent='',index='end',iid=4,text='',
-        values=('C16',descritores[4][0],descritores[4][1],descritores[4][2], descritores[4][3]))
+        values=('C16',descritores[4][0],descritores[4][1],descritores[4][2]))
 
         tabelaDeDescritores.pack()
         
@@ -203,8 +203,9 @@ class main:
 
     def obter_descritores_das_imagens(self, matrizesDeTodasAsImagens):
         try:
-            descritoresTodasAsImagens = open("dataset.pkl", "rb")
-            descritoresTodasAsImagens = np.array(pickle.load(descritoresTodasAsImagens))
+            descritoresTodasAsImagens_arquivo = open("dataset.pkl", "rb")
+            descritoresTodasAsImagens = np.array(pickle.load(descritoresTodasAsImagens_arquivo))
+            descritoresTodasAsImagens_arquivo.close()
         except:
             showinfo(message="Obtendo descritores...")
             descritoresTodasAsImagens = calcula_descritores_varias_imagens(matrizesDeTodasAsImagens)
@@ -213,8 +214,10 @@ class main:
 
     def obter_matrizes_coocorrencia(self):
         try:
-            matrizesDeTodasAsImagens = open("dataset_matrizes.pkl", "rb")
-            matrizesDeTodasAsImagens = np.array(pickle.load(matrizesDeTodasAsImagens))
+            matrizesDeTodasAsImagens_arquivo = open("dataset_matrizes.pkl", "rb")
+            matrizesDeTodasAsImagens = np.array(pickle.load(matrizesDeTodasAsImagens_arquivo))
+            matrizesDeTodasAsImagens_arquivo.close()
+
         except:
             diretorioImagens = self.selecionar_diretorio_imagens()
             showinfo(message="Obtendo matrizes de coocorrência...")
@@ -255,7 +258,7 @@ class main:
 
 
     def classificar_imagem_svm(self):
-        classeDaImagem = classificar_svm(modelo_rede=self.modelo_svm, descritores=self.descritoresImagemExibida, numero_descritores=3)
+        classeDaImagem = classificar_svm(modelo_svm=self.modelo_svm, descritores=self.descritoresImagemExibida, numero_descritores=3)
         mensagem = "A imagem pertence à classe de BIRAD " + str(classeDaImagem)
         showinfo(message=mensagem)
 
